5777938c8ff06a15ea924db8e877690275028f13
Prachi Diwakar Singare <131350738+prachisingare@users.noreply.github.com>
2025-09-09 23:18:28 +0530

    Add files via upload

---
+++ STUDENT REGISTRATION USING PYTHON + SQL/database_creation.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+conn = mysql.connector.connect(host='localhost', user='root', password='2004')
+
+if conn.is_connected():
+    print('connection established')
+
+
+
+mycursor = conn.cursor()
+
+mycursor.execute('create database pythondb')
+
+print(mycursor)
